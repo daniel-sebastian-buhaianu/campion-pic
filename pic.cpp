91db1e809e0055db9231fec8dcf6fc34ae8ee9e3
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-10-22 13:04:31 +0300

    Update pic.cpp

--- pic.cpp
+++ pic.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <cstring>
+#include <algorithm>
 #define MAX_N 70
 #define MAX_K 40
 #define MAXCIF_X 22
@@ -25,21 +26,125 @@ void creeazaNrMareDinSir(NrMare &, char*);
 void afiseazaNrMare(NrMare);
 void citesteDateIntrare(int &, int &, NrMare*, NrMare*);
 void citesteNrMari(int, NrMare*);
+bool compar(NrMare, NrMare);
+int comparaNrMari(NrMare, NrMare);
+void initializeazaNrMare(NrMare &, int=MAXCIF);
+NrMare sumaNrMari(NrMare, NrMare);
+NrMare diferentaNrMari(NrMare, NrMare);
 // functia principala
 int main()
 {
 	int n, k, i;
-	NrMare x[MAX_N], v[MAX_K];
+	NrMare x[MAX_N], v[MAX_K], s;
 	citesteDateIntrare(n, k, x, v);
+	initializeazaNrMare(s, MAXCIF+2);
 	for (i = 0; i < n; i++)
 	{
-		afiseazaNrMare(x[i]);
+		s = sumaNrMari(x[i], s); // s += x[i]
 	}
-	for (i = 0; i < k; i++)
+	sort(v, v+k, compar);
+	for (i = 0; i < k && comparaNrMari(s, v[i]) >= 0; i++)
 	{
-		afiseazaNrMare(v[i]);
+		s = diferentaNrMari(s, v[i]); // s -= v[i]
 	}
+	fout << i;
 	return 0;
+}
+NrMare diferentaNrMari(NrMare a, NrMare b)
+{
+	// d = a - b
+	int i, t, val;
+	NrMare d;
+	for (i = b.nrcif; i < a.nrcif; i++)
+	{
+		b.cif[i] = 0;
+	}
+	for (t = i = 0; i < a.nrcif; i++)
+	{
+		val = a.cif[i]-b.cif[i]+t;
+		if (val < 0)
+		{
+			d.cif[i] = val+10;
+			t = -1;
+		}
+		else
+		{
+			d.cif[i] = val;
+			t = 0;
+		}
+	}
+	// ignor zerourile nesemnificative si actualizez nr. de cifre al lui d
+	for (d.nrcif = a.nrcif; d.nrcif > 1 && d.cif[d.nrcif-1] == 0; d.nrcif--);
+	return d;
+}
+NrMare sumaNrMari(NrMare a, NrMare b)
+{
+	// s = a + b 
+	int i, t, val;
+	NrMare s;
+	// completeaza numarul cel mai mic cu zerouri nesemnificative
+	if (a.nrcif < b.nrcif)
+	{
+		for (i = a.nrcif; i < b.nrcif; i++)
+		{
+			a.cif[i] = 0;
+		}
+		s.nrcif = b.nrcif;
+	}
+	else
+	{
+		for (i = b.nrcif; i < a.nrcif; i++)
+		{
+			b.cif[i] = 0;
+		}
+		s.nrcif = a.nrcif;
+	}
+	// aduna cele doua numere si retine rezultatul in s
+	for (t = i = 0; i < s.nrcif; i++)
+	{
+		val = a.cif[i]+b.cif[i]+t;
+		s.cif[i] = val%10;
+		t = val/10;
+	}
+	if (t)
+	{
+		s.cif[s.nrcif++] = t;
+	}
+	return s;
+}
+void initializeazaNrMare(NrMare & a, int NRMAXCIF)
+{
+	a.nrcif = 1;
+	for (int i = 0; i < NRMAXCIF; i++)
+	{
+		a.cif[i] = 0;
+	}
+}
+int comparaNrMari(NrMare a, NrMare b)
+{
+	if (a.nrcif < b.nrcif)
+	{
+		return -1;
+	}
+	if (a.nrcif > b.nrcif)
+	{
+		return 1;
+	}
+	int i;
+	for (i = a.nrcif-1; i >= 0 && a.cif[i] == b.cif[i]; i--);
+	if (i < 0)
+	{
+		return 0;
+	}
+	if (a.cif[i] < b.cif[i])
+	{
+		return -1;
+	}
+	return 1;
+}
+bool compar(NrMare a, NrMare b)
+{
+	return comparaNrMari(a, b) < 0;
 }
 void citesteNrMari(int n, NrMare* x)
 {
@@ -95,3 +200,4 @@ void afiseazaNrMare(NrMare nr)
 	}
 	fout << '\n';
 }
+// scor 90
